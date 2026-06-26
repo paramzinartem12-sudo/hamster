3d56b65d68323d6b613ba2912486b5b9a6bb5b70
paramzinartem12-sudo <paramzinartem12@gmail.com>
2026-06-27 01:11:38 +0300

    а
    а

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'hamster-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json'
+];
+
+// Установка воркера и кэширование файлов
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Активация и удаление старого кэша
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Перехват запросов для работы оффлайн
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
